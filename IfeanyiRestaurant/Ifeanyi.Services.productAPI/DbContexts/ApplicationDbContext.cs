cd2dd1245aaf05338bb89ead01dcb7a05fbf6536
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-13 01:11:14 +0100

    Added an MVC fronted project and a Products API project

---
+++ IfeanyiRestaurant/Ifeanyi.Services.productAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ifeanyi.Services.productAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
