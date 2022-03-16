92fa9063893a75fa6485e0974382cf1a3b518d13
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-16 06:19:25 +0100

    Added models to the web project for consuming the Product service

---
+++ IfeanyiRestaurant/Ifeanyi.Web/Helpers/StaticDetails.cs
@@ -0,0 +1,14 @@
+﻿namespace Ifeanyi.Web.Helpers
+{
+    public static class StaticDetails
+    {
+        public static string  ProductAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
---
+++ IfeanyiRestaurant/Ifeanyi.Web/Models/ResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Ifeanyi.Web.Models
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = string.Empty;
+        public List<string> ErrorMessages { get; set; }
+    }
+}
