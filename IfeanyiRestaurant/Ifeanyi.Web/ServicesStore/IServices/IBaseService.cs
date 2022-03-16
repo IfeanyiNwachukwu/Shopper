700d4f339ac63b1eb903dc77206d0976d6bfb115
Ifeanyi Nwachukwu <ifeanyin@xceed365.com>
2022-03-16 08:24:50 +0100

    Base service rewrite

---
+++ IfeanyiRestaurant/Ifeanyi.Web/ServicesStore/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Ifeanyi.Web.Models;
+
+namespace Ifeanyi.Web.ServicesStore.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDTO responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
---
+++ IfeanyiRestaurant/Ifeanyi.Web/ServicesStore/Services/BaseService.cs
@@ -0,0 +1,77 @@
+﻿using Ifeanyi.Web.Helpers;
+using Ifeanyi.Web.Models;
+using Ifeanyi.Web.ServicesStore.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Ifeanyi.Web.ServicesStore.Services
+{
+    public class BaseService : IBaseService
+    {
+       
+        public IHttpClientFactory _httpClient { get; set; }
+        public ResponseDTO responseModel { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            responseModel = new ResponseDTO();
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = _httpClient.CreateClient("IfeanyiAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                        Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case StaticDetails.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case StaticDetails.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case StaticDetails.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch (Exception ex)
+            {
+
+                var DTO = new ResponseDTO
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string>() { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(DTO);
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDTO;
+            }
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
